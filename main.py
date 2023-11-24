a860473dc680f060ecdc24811b28835a28e16c63
MateusMatana <mateusmatana12@gmail.com>
2023-11-24 15:49:06 -0300

    Parte 2 - Tabuada

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def index():
    return { "data" : "Bem vindo!!!" }
 
 
+#Parte 1 - Quadrados
+
+
 @app.get("/quadrado")
 def listaQuadrados(max : Optional[int] = 10):
    quadrados = []
@@ -20,3 +23,16 @@ def listaQuadrados(max : Optional[int] = 10):
    }
 
 
+#Parte 2 - 
+
+
+@app.get("/tabuada/{n}")
+def listaTabuada(n : int, start : Optional[int] = 1 , end : Optional[int] = 10):
+   tabuada = []
+   for i in range(start, end+1):
+       tabuada.append(n*i)
+  
+   return {
+       "número" : n,
+       "tabuada" : tabuada
+   }
