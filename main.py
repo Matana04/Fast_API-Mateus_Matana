c9949c42d6a35b6ae29e685ceed0c1e0ba7feb32
MateusMatana <mateusmatana12@gmail.com>
2023-11-24 15:19:45 -0300

    Fast_API-Mateus_Matana
    Parte 1 - Quadrados

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("teste")
+from fastapi import FastAPI
+from typing import Optional
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+   return { "data" : "Bem vindo!!!" }
+
+
+@app.get("/quadrado")
+def listaQuadrados(max : Optional[int] = 10):
+   quadrados = []
+   for i in range(1, max+1):
+       quadrados.append(i**2)
+  
+   return {
+       "max" : max,
+       "quadrados" : quadrados
+   }
+
+
